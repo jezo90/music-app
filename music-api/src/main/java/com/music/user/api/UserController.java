c7b7fec47555645ffeab455d9b6599ff394356f0
Jezo <kamiljezovvski@gmail.com>
2022-01-27 19:48:16 +0100

    clean code refactor

--- music-api/src/main/java/com/music/user/api/UserController.java
+++ music-api/src/main/java/com/music/user/api/UserController.java
@@ -17,7 +17,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
@@ -40,7 +43,7 @@ class UserController {
     @PostMapping("/login")
     public ResponseEntity<UserToken> generateToken(@RequestBody UserRequest userRequest){
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(userRequest.getLogin(), userRequest.getPassword()));
+                new UsernamePasswordAuthenticationToken(userRequest.login(), userRequest.password()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
