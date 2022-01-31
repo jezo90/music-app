621a7c9423b8650e445dee75ddfcfebeb54b85c4
Jezo <kamiljezovvski@gmail.com>
2022-01-31 13:02:04 +0100

    Register component

--- music-angular/src/app/app.module.ts
+++ music-angular/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {NavComponent} from "./components/nav/nav.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
