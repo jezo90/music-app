4f6ca8f063852754baafef0d086d47e40cf103ae
Jezo <kamiljezovvski@gmail.com>
2022-01-31 13:02:04 +0100

    Register component

--- music-angular/src/app/components/nav/nav.component.ts
+++ music-angular/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from "../../services/tokenStorage/token-storage.service";
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tokenStorage: TokenStorageService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  handleLogout() {
+    this.tokenStorage.signOut();
   }
 
 }
